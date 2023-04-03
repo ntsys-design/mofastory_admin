4025f680b62d124ddc528bae47955e51211d4d7e
yu jieun <yje3454@ntsys.co.kr>
2023-04-03 16:59:13 +0900

    #0403_통계관리작업

--- common/js/publish.js
+++ common/js/publish.js
@@ -42,4 +42,9 @@ $(document).ready(function() {
   
   })
   
-
+// 0403  통계관리 페이지 
+// 조건 선택
+$('.select_m li button').on('click',function(){
+  $('.select_m li button').removeClass('active')
+  $(this).addClass('active')
+})
